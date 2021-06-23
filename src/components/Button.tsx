6ee9a6ccc1a46b03bf445c673b91babf538f10dd
Kairon Ferreira <gelsonkairon.10@gmail.com>
2021-06-22 21:12:18 -0300

    Implantação de autenticação com Google
    Implantação da autenticação com Google através do firebase.
    
    Criação de Contexts e Hooks.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react"
+import {ButtonHTMLAttributes} from 'react';
+import '../styles/button.scss';
 
-export function Button(){
-    const [counter, setCounter] = useState(0);
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-    function increment(){
-        setCounter(counter + 1);
-
-    }
-
+export function Button (props: ButtonProps){
+    
     return(
-        <button onClick={increment}>{counter}</button>
+        <button className="button" {...props} />
     )
 }
